b864dd2b5f1cc84079f7b1f2eab37879a757b1bc
Shubham Singh Bhadouria <96174262+Shubhambhadouria@users.noreply.github.com>
2022-06-02 21:10:30 +0530

    Update AllStudents.java

--- day10q2/AllStudents.java
+++ day10q2/AllStudents.java
@@ -1,17 +1,55 @@
 package day10q2;
 
+import java.util.Scanner;
+
 public class AllStudents {
 
 	public static void main(String[] args) {
 		
-		HistoryStudent h1 = new HistoryStudent("Shubham","276 Durgesh Vihar", 67, 80);
-		double x = h1.getPercentage();
-		System.out.println(x);
 		
-		ScienceStudent s1 = new ScienceStudent("Shubham","276 Durgesh Vihar", 67, 80, 69);
+		
+		
+		Scanner sc = new Scanner(System.in);
+		System.out.println("Enter History Student Name : ");
+		String name = sc.next();
+		
+		sc.nextLine();
+		System.out.println("Enter History Student Address : ");
+		String address = sc.nextLine();
+	
+		System.out.println("Enter History Marks : ");
+		int hmarks = sc.nextInt();
+		
+		System.out.println("Enter Civics Marks : ");
+		int cmarks = sc.nextInt();
+		
+		HistoryStudent h1 = new HistoryStudent(name,address, hmarks, cmarks);
+		double x = h1.getPercentage();
+		System.out.println("The Percentage of Science Student is : "+x);
+		
+		
+		System.out.println("Enter Science Student Name : ");
+		String name1 = sc.next();
+		
+		sc.nextLine();
+		System.out.println("Enter Science Student Address : ");
+		String address1 = sc.nextLine();
+	
+		System.out.println("Enter Physics Marks : ");
+		int hmark1 = sc.nextInt();
+		
+		System.out.println("Enter Chemistry Marks : ");
+		int cmarks1 = sc.nextInt();
+		
+		System.out.println("Enter Maths Marks : ");
+		int cmarks2 = sc.nextInt();
+		
+		
+		ScienceStudent s1 = new ScienceStudent(name1,address1,hmark1,cmarks1,cmarks2);
 		double y = s1.getPercentage();
-		System.out.println(y);
+		System.out.println("The Percentage of Science Student is : "+y);
 
 	}
 
 }
+
